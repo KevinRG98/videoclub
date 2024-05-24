62fbb9454293a07b9aedf374ea5ffa6085f17836
KevinRG98 <kriosg@unitru.edu.pe>
2024-05-24 14:50:37 -0500

    Github-videoclub

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,39 @@
 <?php
-/*
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-*/
-
-//Petición por get
-
-Route::get('/', function () {
-	return '¡Hola Mundo desde Laravel..!';
+    return view('home');
+    //return 'Pantalla principal';
 });
 
-//Petición por post
-
-Route::post('persona/nuevo', function(){
-	return '¡Hola Mundo desde Laravel..!';
+Route::get('login', function () {
+    return view('auth.login');
+    //return 'Login usuario';
 });
 
-//Petición por put
-
-Route::put('persona/nuevo', function () { 
-	//
+Route::get('logout', function () {
+    //return view('');
+    return 'Logout usuario';
 });
 
-//Petición por delete
-
-Route::delete('persona/borrar', function () {
+Route::get('catalog', function () {
+    return view('catalog.index');
+    //return 'Listado Películas';
 });
 
+Route::get('catalog/show/{id}', function ($id) {
+    return view('catalog.show', ['id' => $id]);
+    //return 'Vista detalle película ' . $id;
+});
 
+Route::get('catalog/create', function () {
+    return view('catalog.create');
+    //return 'Añadir película';
+});
+
+Route::get('catalog/edit/{id}', function ($id) {
+    return view('catalog.edit', ['id' => $id]);
+
+    //return 'Modificar película ' . $id;
+});
