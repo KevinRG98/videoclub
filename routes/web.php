6b9852fa08ea44e5b12260e0d79b9c3058046a5b
KevinRG98 <kriosg@unitru.edu.pe>
2024-05-24 01:08:57 -0500

    A

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,33 @@
 <?php
-
+/*
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
+//Petición por get
 
+Route::get('/', function () {
+	return '¡Hola Mundo desde Laravel..!';
+});
+
+//Petición por post
+
+Route::post('persona/nuevo', function(){
+	return '¡Hola Mundo desde Laravel..!';
+});
+
+//Petición por put
+
+Route::put('persona/nuevo', function () { 
+	//
+});
+
+//Petición por delete
+
+Route::delete('persona/borrar', function () {
+});
 
 
